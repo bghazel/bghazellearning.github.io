219f3f684a1d707b2bd29836894fb35ed0cd42e2
bghazel <ben.g.hazel7@gmail.com>
2025-08-25 16:05:05 -0400

    created dism file and other small adjustments

---
+++ PowerShell/dism-sfc.ps1
@@ -0,0 +1,4 @@
+#Plan here is to run DISM /Online /cleanup-image /restorehealth and then sfc /scannow in one click.
+#Once that is working attempt to have the output logged in easily accesible file.
+
+
