3a7cbe6000fd6ad9314aba1046601cce4b178054
bghazel <ben.g.hazel7@gmail.com>
2025-08-25 15:58:40 -0400

    log update

--- PowerShell/helloworldvscodesetup.ps1
+++ PowerShell/helloworldvscodesetup.ps1
@@ -1,3 +1,2 @@
 write-host "I am testing and troubleshooting setting up VSCode with powershell and remote github repo"
 Write-Host "It looks like I cant run and debug powershell commands with remote repo so I will have to clone the repo on each computer."
-write-host "I also had to add an exception in threalocker for my computer to allow it run ps scripts without express permission for each one. I layered the full path with a wild card variable, the process path and the creation path to make it more secure."
