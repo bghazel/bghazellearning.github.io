c69735d8c5f38854590d990218cfb220305b6625
bghazel <ben.g.hazel7@gmail.com>
2025-08-25 16:25:45 -0400

    test

--- PowerShell/dism-sfc.ps1
+++ PowerShell/dism-sfc.ps1
@@ -2,3 +2,7 @@
 #Once that is working attempt to have the output logged in easily accesible file.
 
 
+#Quick Google search brings me this. Will test then break it down to understand.
+
+Start-Process -FilePath “${env:Windir}\System32\SFC.EXE” -ArgumentList “/scannow” 
+Read-Host -Prompt "Press Enter to Exit"
