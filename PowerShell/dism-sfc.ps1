ffc4f1bbe1ea9504bce253c7fb6efb587f6d76ab
bghazel <ben.g.hazel7@gmail.com>
2025-08-26 16:52:17 -0400

    Whole bunch of changes to make the full cleanup/tuneup script work. turns out you cant use curly "" only the straight ones. I'm not even sure how I managed to do the curly ones in the first place.

--- PowerShell/dism-sfc.ps1
+++ PowerShell/dism-sfc.ps1
@@ -1,14 +1,17 @@
 #Plan here is to run DISM /Online /cleanup-image /restorehealth and then sfc /scannow in one click.
 #Once that is working attempt to have the output logged in easily accesible file.
-
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
 
 #Quick Google search brings me this. Will test then break it down to understand.
 #Start-process is a good find. can be used for nearly anything - ${env:Windir} refers to the windows environment its being run in and references it to make more universal path. 
 #-argumentlist allows for entry of the various options a program might have. -verb RunAs to run the called program as admin.
 
-Start-Process -FilePath "dism.exe" -ArgumentList "/online /cleanup-image /restorehealth" -Wait -Verb RunAs
+#Start-Process -FilePath "dism.exe" -ArgumentList "/online /cleanup-image /restorehealth" -Wait -Verb RunAs
 
-Start-Process -FilePath “${env:Windir}\System32\SFC.EXE” -ArgumentList “/scannow”  -Wait -Verb RunAs
+Start-Process -FilePath "${env:Windir}\System32\SFC.EXE" -ArgumentList "/scannow" -Wait -Verb RunAs
 
 Read-Host -Prompt "Scans have finished running. Press Enter to close this window now"
 
--- PowerShell/fullcleanup-tuneup.ps1
+++ PowerShell/fullcleanup-tuneup.ps1
@@ -1,16 +1,26 @@
-#This script is to combine the various cleanup/tuneup scripts created into one for a one button press cleanup process.
+#This script is to combine the various cleanup/tuneup scripts created into one for a one button press cleanup process. Using functions for the first time to try and organize well.
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
     exit;
 }
 
+#Winget Upgrade
+function wingetupgrade{
+winget upgrade --all --force
+}
+
+#Disk Cleanup
+function diskcleanup{
+         Write-Host "Starting Disk Cleanup Function"
+
 $cleanmgrcheck = $null
 
-cleanmgr /verylowdisk
+$downloadcheck = Read-Host -Prompt "Do you want to clear your downloads folder? Y\N"
 
-#next goal is to have it close automatically so we can run without user interference
+cleanmgr /verylowdisk
 while ($null -eq $cleanmgrcheck) {
+   #Closes the popup automatically
    $cleanmgrcheck = Get-Process | Where-Object {$_.MainWindowTitle -eq "Disk Space Notification"} | Select-Object MainWindowTitle
    }
          
@@ -30,7 +40,6 @@ Remove-Item -Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Recent\*" 
          Write-Host "Cleared recent Items From This profile"
 
 #Clear Downloads Folder
-$downloadcheck = Read-Host -Prompt "Do you want to clear your downloads folder? Y\N"
 if($downloadcheck -eq "Y"){
    Remove-Item -Path "$env:USERPROFILE\Downloads\*" -Force -Recurse
          Write-Host "Cleared the Downloads Folder From this profile"
@@ -41,22 +50,98 @@ else{
 
 
 #Wise Disk Cleaner // Will want to check for pop up to close it then move on to registry cleaner.
+         Write-Host "Starting Wise Disk Cleaner. Please Wait..."
 Start-Process -FilePath "C:\Program Files (x86)\Wise\Wise Disk Cleaner\WiseDiskCleaner.exe" -argumentlist "-a" -wait
          Write-Host "Finished Wise Disk Cleaner"
 
 #Wise Registry Cleaner
+         Write-Host "Starting Wise Registry Cleaner. Please Wait..."
 Start-Process -FilePath "C:\Program Files (x86)\Wise\Wise Registry Cleaner\WiseregCleaner.exe" -argumentlist "-a -safe" -wait
          Write-Host "Finished Wise Registry Cleaner"
 
 
-Read-Host -prompt "Disk Cleanup Complete: Press Enter to proceed to SFC/DISM Corruption Scans"
+Write-Host "Disk Cleanup Complete"
+
+}
+
+#DISM // SFC Scans
+function sfcdism {
+
+         Write-Host "Starting SFC/DISM Function"
+
+#Start-Process -FilePath "dism.exe" -ArgumentList "/online /cleanup-image /restorehealth" -Wait -Verb RunAs
+
+Start-Process -FilePath "${env:Windir}\System32\SFC.EXE" -ArgumentList "/scannow"  -Wait -Verb RunAs
+
+Write-Host "DISM\SFC Corruption Scans have finished running."
+}
+
+#Create PCInfo File
+function PCInfotxt{
+      
+         Write-Host "Creating PCInfo File in C:\ Drive"
+
+    #most pertinent info to access
+
+"Quick Access Information" >> C:\pcinfo.txt
+Get-ComputerInfo -Property CSName,CSModel,OsName,WindowsInstallDateFromRegistry | Format-List >> C:\pcinfo.txt
+Get-WmiObject win32_bios | Select-Object SerialNumber >> C:\pcinfo.txt
+"IP:Address " >> C:\pcinfo.txt ;"----------" >> C:\pcinfo.txt
+(@(Get-WmiObject Win32_NetworkAdapterConfiguration | Select-Object -ExpandProperty IPAddress) -like "*.*") >> C:\pcinfo.txt
+"">>C:\pcinfo.txt ; "Date Configured" >> C:\pcinfo.txt ; "----------" >> C:\pcinfo.txt ; get-date >> C:\pcinfo.txt
+"Hard Drive Information" >> C:\pcinfo.txt ; get-volume >> C:\pcinfo.txt
+"Installed Applications" >> C:\pcinfo.txt ; Get-Package >> C:\pcinfo.txt
+
+   #Just straight sysinfo command. Harder to parse but more information
+"Further Information" >> C:\pcinfo.txt
+systeminfo >> C:\pcinfo.txt
+}  
 
 
 
-#DISM // SFC Scanss
 
-Start-Process -FilePath "dism.exe" -ArgumentList "/online /cleanup-image /restorehealth" -Wait -Verb RunAs
 
-Start-Process -FilePath “${env:Windir}\System32\SFC.EXE” -ArgumentList “/scannow”  -Wait -Verb RunAs
+#Function Selection
+while ($functionselection -ne 6) {
+   
+Write-Host "Welcome to Ben's TechWizard Cleanup/Tuneup Script"
+Write-Host "There are several functions please enter which function(s) you would like to use (This prompt will repeat upon completion of a function)"
+Write-Host "(1) Winget Upgrade"
+Write-Host "(2) Disk Cleanup"
+Write-Host "(3) DISM/SFC Scans"
+Write-Host "(4) PCInfo File Creation"
+Write-Host "(5) All of the above"
+Write-Host "(6) Exit"
+$functionselection = Read-Host -Prompt "Enter a number (1-6)"
 
-Read-Host -Prompt "Scans have finished running. Press Enter to close this window now"
+   #Winget Upgrade
+   if ($functionselection -eq 1) {
+      wingetupgrade
+   }
+
+   #Windows Disk Cleanup
+   if ($functionselection -eq 2) {
+      diskcleanup
+   }
+
+   #DISM // SFC Scans
+   if ($functionselection -eq 3) {
+      sfcdism
+   }
+
+   #Create PCInfo File
+   if ($functionselection -eq 4) {
+      PCInfotxt
+   }
+
+   #All
+   if ($functionselection -eq 5) {
+      wingetupgrade
+      diskcleanup
+      sfcdism
+      PCInfotxt
+   }
+   Write-Host "" #Used to break up the repeats
+}
+
+Read-Host -Prompt "Thank you for using Ben's TechWizard Cleanup/Tuneup Script (Press Enter to Exit)"
