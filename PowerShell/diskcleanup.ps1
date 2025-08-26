b63d557dfa6091763e4e212dc2267acf8b8f21e2
bghazel <ben.g.hazel7@gmail.com>
2025-08-26 12:28:16 -0400

    added trim/defrag to diskcleanup script

--- PowerShell/diskcleanup.ps1
+++ PowerShell/diskcleanup.ps1
@@ -1,6 +1,12 @@
 #I'd like to have this one run the windows disk cleanup program with all boxes selected automatically then close after a prompt letting the user know its finished
 #The hope is to reduce the steps in our final cleanup/tuneup checklist into a single powershell script
 
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
+
 $cleanmgrcheck = $null
 #Write-Host "checking $cleanmgrcheck"
 
@@ -14,4 +20,9 @@ while ($null -eq $cleanmgrcheck) {
 }
 #Write-Host "pre kill"
    Get-Process | Where-Object {$_.MainWindowTitle -eq "Disk Space Notification"} | Stop-Process -Force
+
+#Optimize/defrag drive
+Optimize-Volume -DriveLetter C -ReTrim -Defrag
+
+
 Read-Host -prompt "Disk Cleanup Complete: Press Enter to exit"
