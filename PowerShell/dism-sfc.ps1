2e14113a3b8ca36481611ea6456ae0fe58bd5740
bghazel <ben.g.hazel7@gmail.com>
2025-08-26 08:32:44 -0400

    finished dism/sfc scan file

--- PowerShell/dism-sfc.ps1
+++ PowerShell/dism-sfc.ps1
@@ -3,6 +3,9 @@
 
 
 #Quick Google search brings me this. Will test then break it down to understand.
-
-Start-Process -FilePath “${env:Windir}\System32\SFC.EXE” -ArgumentList “/scannow” 
-Read-Host -Prompt "Press Enter to Exit"
+#Start-process is a good find. can be used for nearly anything - ${env:Windir} refers to the windows environment its being run in and references it to make more universal path. 
+#-argumentlist allows for entry of the various options a program might have. -verb RunAs to run the called program as admin.
+Start-Process -FilePath "dism.exe" -ArgumentList "/online /cleanup-image /restorehealth" -Wait -Verb RunAs
+Start-Process -FilePath “${env:Windir}\System32\SFC.EXE” -ArgumentList “/scannow”  -Wait -Verb RunAs
+Read-Host -Prompt "Scans have finished running. Press Enter to close this window now"
+#-RedirectStandardOutput C:\
