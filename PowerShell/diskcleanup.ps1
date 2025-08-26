3dcab8256fc573abc291dfef470fdd66b6d9fa1f
bghazel <ben.g.hazel7@gmail.com>
2025-08-26 14:24:02 -0400

    created a full tuneup script and edited the diskcleanup one to add wise cleanup functions

--- PowerShell/diskcleanup.ps1
+++ PowerShell/diskcleanup.ps1
@@ -1,6 +1,7 @@
 #I'd like to have this one run the windows disk cleanup program with all boxes selected automatically then close after a prompt letting the user know its finished
 #The hope is to reduce the steps in our final cleanup/tuneup checklist into a single powershell script
 
+
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
     exit;
@@ -8,19 +9,33 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 
 $cleanmgrcheck = $null
 
-cleanmgr /verylowdisk -Wait
+cleanmgr /verylowdisk
 #next goal is to have it close automatically so we can run without user interference
 
 while ($null -eq $cleanmgrcheck) {
    $cleanmgrcheck = Get-Process | Where-Object {$_.MainWindowTitle -eq "Disk Space Notification"} | Select-Object MainWindowTitle
    }
-
+         Write-Host "this is a test"
 Get-Process | Where-Object {$_.MainWindowTitle -eq "Disk Space Notification"} | Stop-Process -Force
+
 
 #Optimize/defrag drive
 Optimize-Volume -DriveLetter C -ReTrim -Defrag
+         Write-Host "finished defrag"
+
 
 #Empty Recycle Bin
 Clear-RecycleBin -Force
+         Write-Host "finished recycle"
+
+
+
+#Wise Disk Cleaner // Will want to check for pop up to close it then move on to registry cleaner.
+Start-Process -FilePath "C:\Program Files (x86)\Wise\Wise Disk Cleaner\WiseDiskCleaner.exe" -argumentlist "-a" -wait
+
+#Wise Registry Cleaner
+Start-Process -FilePath "C:\Program Files (x86)\Wise\Wise Registry Cleaner\WiseregCleaner.exe" -argumentlist "-a -safe" -wait
+
+
 
 Read-Host -prompt "Disk Cleanup Complete: Press Enter to exit"
