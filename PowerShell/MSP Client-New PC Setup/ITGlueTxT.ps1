d18c4983ebd09c8ec4f4c3378fbd559765565b71
bghazel <ben.g.hazel7@gmail.com>
2025-08-27 16:44:07 -0400

    Finished the ITGlueTxT script and started the setcompsettings script.

--- PowerShell/MSP Client-New PC Setup/ITGlueTxT.ps1
+++ PowerShell/MSP Client-New PC Setup/ITGlueTxT.ps1
@@ -6,31 +6,49 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     exit;
 }
 
-#Preliminary Info (Technician, Company, User, Date Started Computer Info)
+#Defining Variables
+$script:comptype = $null
+$script:compmake = $null
+$script:compmodel = $null
+$script:serialnumber = $null
+$script:datetime = $null
 
 #Check Computer Type
 function compinfo{
-    $comptype = (get-computerinfo).CsPCSystemType         # more simplified comp info
+    $script:comptype = (get-computerinfo).CsPCSystemType         # more simplified comp info
     #$comptype = get-ciminstance -ClassName Win32_SystemEnclosure | Select-Object ChassisTypes          More complex computer type info (laptop,desktop,notebook,sff,etc)
-    $compmake = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object Manufacturer
-    $compmodel = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object Model
-    $serialnumber = Get-CimInstance -classname Win32_BIOS | Select-Object SerialNumber
-    $datetime = Get-Date
+    $script:compmake = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object Manufacturer
+    $script:compmodel = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object Model
+    $script:serialnumber = Get-CimInstance -classname Win32_BIOS | Select-Object SerialNumber
+    $script:datetime = Get-Date
     if($comptype -eq "mobile"){
-        "Computer Type: Laptop">> C:\ITGlueInfo.txt
         $comptype = "Laptop"
       }
     if($comptype -eq "desktop"){
-        "Computer Type: Desktop">> C:\ITGlueInfo.txt
         $comptype = "Desktop"
       }
 }
 
 
-Write-Host "Welcome to the MSP Client: New PC Setup Checklist Script"
-$technician = Read-Host -Prompt "Technician Name:"
-    "Technician: $technician" >> C:\ITGlueInfo.txt
-$enduser = Read-Host -Prompt "End Users Name"
-    "End User: $enduser" >> C:\ITGlueInfo.txt
-$compname = Read-Host -Prompt "Desired Computer Name (ORG-##TT ie: PAL-72LT)"
 
+#Required Inputs
+Write-Host "Welcome to the MSP Client: New PC Setup Checklist Script"
+Write-Host "All information needed to put into IT Glue will be put into ITGlueinfo.txt in the C Drive"
+  $technician = Read-Host -Prompt "Technician Name:"
+  $enduser = Read-Host -Prompt "End Users Name"
+  $password = Read-Host -Prompt "End Users Password"
+  $compname = Read-Host -Prompt "Desired Computer Name (ORG-##TT ie: PAL-72LT)"
+
+#Call Function
+  compinfo
+
+#Writing to file
+"Title: MSP Client: New PC Setup - <$compname> - <$enduser>" >> C:\ITGlueInfo.txt
+"Technician: $technician" >> C:\ITGlueInfo.txt
+"End User: $enduser" >> C:\ITGlueInfo.txt
+"Date Setup Started $datetime" >> C:\ITGlueInfo.txt
+"Computer Type: $comptype" >> C:\ITGlueinfo.txt
+"Computer Info: Make: $compmake, Model: $compmodel, Serial Number: $serialnumber" >> C:\ITGlueInfo.txt
+"Computer Name: $compname" >> C:\ITGlueInfo.txt
+"Username: $enduser" >> C:\ITGlueInfo.txt
+"Password: $password" >> C:\ITGlueInfo.txt
