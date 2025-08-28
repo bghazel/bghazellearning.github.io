17836e165554603e3ed6891bdd8f5b8616cc547c
bghazel <ben.g.hazel7@gmail.com>
2025-08-28 16:29:02 -0400

    Added wallpaper download to AppDownloads

--- PowerShell/MSP Client-New PC Setup/AppDownloads.ps1
+++ PowerShell/MSP Client-New PC Setup/AppDownloads.ps1
@@ -23,30 +23,37 @@ switch ($script:company) {
 
     "ACS" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=bcd711e04e534afcb0240b65c17e090e"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/ACS_Wallpaper.bmp"
     }
 
     "AHT" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=da36bb68171f46d1830ab41f01d08350"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/AmericanHerosWallpaper.jpg"
     }
 
     "AUTH" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=7d8e106872264a7ab5cee8b035a59921"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/Authentic%20Title%20Wallpaper.jpg"
     }
 
     "BIC" {
         $url = "https://na1vsa14.kaseya.net/deploy/#/76182152372222312513798251/bic.main"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/BryantIndustrialWallpaper.bmp"
     }
 
     "CAD" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=ef1af3ba9c5446c8b7c88ac3a1bb4603"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/CaryAudioWallpaper.bmp"
     }
 
     "FRE" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=ec5c337984f64571ab56f112114d37ad"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/FRE_Wallpaper.jpg"
     }
 
     "NRLG" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=9f6823e88b634176b9f6628474064be0"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/NRLG_Wallpaper.bmp"
     }
 
     "PAL" {
@@ -58,10 +65,12 @@ switch ($script:company) {
             3 { $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=51f369a453b444b9b39930a95ab01b70" }
             default { Write-Host "Invalid PAL option selected."; return }
         }
+        $bkg = "http://208.106.176.175/Images/Wallpapers/PalominoGroupWallpaper.bmp"
     }
 
     "QBI" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=7985a846c0a24ea1957e5729090e8568"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/QBIwallpaper.jpg"
     }
 
     "SHO" {
@@ -73,26 +82,32 @@ switch ($script:company) {
             3 { $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=a75a8ca6848e4235a228c5f7522f0a5f" }
             default { Write-Host "Invalid SHO option selected."; return }
         }
+        $bkg = "http://208.106.176.175/Images/Wallpapers/ShoafLawWallpaper.bmp"
     }
 
     "SKY" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=c7d83a62f72843b3afad6fad91b84dc9"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/Skyrock%20Wallpaper.jpg"
     }
 
     "TWIZ" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=07c71a86ed3745c8981a054a70bd4c51"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/Twiz%20Local%20Profile%20Wallpaper%20(new).jpg"
     }
 
     "VLT" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=138bf200d88a43b5ad42d4dc757505a3"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/VLT_Wallpaper.jpg"
     }
 
     "VIL" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=fc675d22a6bf4a1f95db490f6f02e1e8"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/VillageLawGroupLogo.jpg"
     }
 
     "VLS" {
         $url = "https://twiznc.vsax.net/installer?type=windows_agent_x64&secret=e7a175a4fbae4dd29a3b36e09b420ae5"
+        $bkg = "http://208.106.176.175/Images/Wallpapers/Village%20Surrogacy%20Wallpaper.jpg"
     }
 
     default {
@@ -104,6 +119,9 @@ switch ($script:company) {
 # Run the download if a URL was set
 if ($url) {
     Invoke-WebRequest -Uri $url -OutFile "C:\Users\$($script:enduser)\Downloads\VSAInstaller.msi"
+}
+if($bkg){
+    Invoke-WebRequest -Uri $bkg -OutFile "C:\Users\$($script:enduser)\Downloads\wallpaper.bmp"
 }
 
 }
@@ -120,3 +138,4 @@ function standardweb{
 
 VSAbkg
 Read-Host -Prompt "asdf"
+
