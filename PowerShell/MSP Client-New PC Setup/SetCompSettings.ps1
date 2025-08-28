982948a5a67c2f87c466ed9c00ca7f679203de04
bghazel <ben.g.hazel7@gmail.com>
2025-08-28 10:20:28 -0400

    Added Power settings, maintenence settings, UAC, and time zone to SetCompSettings.ps1 script.

--- PowerShell/MSP Client-New PC Setup/SetCompSettings.ps1
+++ PowerShell/MSP Client-New PC Setup/SetCompSettings.ps1
@@ -6,15 +6,18 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     exit;
 }
 
+#Auto Set Time Zone Based on Location
+$locpermpath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location"
+$autosettime = "HKLM:\SYSTEM\CurrentControlSet\Services\tzautoupdate"
+Set-ItemProperty -Path $locpermpath -Name "Value" -Value "Allow"
+Set-ItemProperty -Path $autosettime -Name "Start" -Value "3"
 
-#Finding what kind of computer (ripped from ITGlue.txt)
+#Finding what kind of computer (ripped from ITGluetxt.ps1)
 $comptype = (get-computerinfo).CsPCSystemType 
 if($comptype -eq "mobile"){
-        "Computer Type: Laptop">> C:\ITGlueInfo.txt
         $comptype = "Laptop"
       }
 if($comptype -eq "desktop"){
-        "Computer Type: Desktop">> C:\ITGlueInfo.txt
         $comptype = "Desktop"
       }
 
@@ -31,3 +34,31 @@ Write-Host "Computer Name Change will be applied on next restart"
 #Disabling Bitlocker Encryption
 Disable-BitLocker -MountPoint "C:"
 
+#UAC
+$UACPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+Set-ItemProperty -Path $UACPath -Name "EnableLUA" -Value "1"
+Set-ItemProperty -Path $UACPath -Name "PromptOnSecureDesktop" -Value "0"
+Set-ItemProperty -Path $UACPath -Name "ConsentPromptBehaviorAdmin" -Value "0"
+
+#Show More Options
+$newoptionspath = "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32"
+New-Item -Path $newoptionspath -Force
+Set-ItemProperty -Path $newoptionspath -Name "(Default)" -Value ""
+Stop-Process -Name explorer -Force 
+
+#Power Settings
+powercfg.exe -SETACTIVE 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+powercfg.exe -x -monitor-timeout-ac 30
+powercfg.exe -x -monitor-timeout-dc 0
+powercfg.exe -x -disk-timeout-ac 120
+powercfg.exe -x -disk-timeout-dc 120
+powercfg.exe -x -standby-timeout-ac 0
+powercfg.exe -x -standby-timeout-dc 0
+powercfg.exe -x -hibernate-timeout-ac 0
+powercfg.exe -x -hibernate-timeout-dc 0
+
+#Schedule Maintenance
+$maintenancepath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Maintenance"
+Set-ItemProperty -Path $maintenancepath -Name "Activation Boundary" -Value "2001-01-01T02:00:00"
+
+#Create End User Profile
