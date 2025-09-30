47b0511ae5bcc970ae8721deb243ebfc707d9b55
bghazel <ben.g.hazel7@gmail.com>
2025-09-30 12:41:54 -0400

    Working on having toolbox install script.

--- PowerShell/MSP Client-New PC Setup/ScriptDownload.ps1
+++
@@ -1,2 +0,0 @@
-#The goal of this script is to Download the usefull scripts that I've made onto every computer in the same spot to make a toolbox.
-
---
+++ PowerShell/MSP Client-New PC Setup/ToolboxDownload.ps1
@@ -0,0 +1,19 @@
+#The goal of this script is to Download the usefull scripts that I've made onto every computer in the same spot to make a toolbox.
+#https://github.com/bghazel/bghazellearning.github.io/tree/main/PowerShell/Toolbox
+
+# Self Elevate
+if (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`""
+    exit
+}
+<#
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/thomasmaurer/demo-cloudshell/master/*.ps1 -OutFile .\helloworld.ps1
+
+https://github.com/bghazel/bghazellearning.github.io/blob/main/PowerShell/Toolbox/WingetInstall.ps1
+https://raw.githubusercontent.com/bghazel/bghazellearning.github.io/refs/heads/main/PowerShell/Toolbox/WingetInstall.ps1
+#>
+
+
+$appname = "WingetInstall"
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bghazel/bghazellearning.github.io/refs/heads/main/PowerShell/Toolbox/$appname.ps1" -OutFile "C:\Toolbox\$appname.ps1"
