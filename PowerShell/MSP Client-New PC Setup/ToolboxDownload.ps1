96720322c8827c71b23a2cf3db4bf9381be3f4c0
bghazel <ben.g.hazel7@gmail.com>
2025-09-30 12:56:17 -0400

    edited list

--- PowerShell/MSP Client-New PC Setup/ToolboxDownload.ps1
+++ PowerShell/MSP Client-New PC Setup/ToolboxDownload.ps1
@@ -12,8 +12,14 @@ Invoke-WebRequest -Uri https://raw.githubusercontent.com/thomasmaurer/demo-cloud
 https://github.com/bghazel/bghazellearning.github.io/blob/main/PowerShell/Toolbox/WingetInstall.ps1
 https://raw.githubusercontent.com/bghazel/bghazellearning.github.io/refs/heads/main/PowerShell/Toolbox/WingetInstall.ps1
 #>
+read-Host "TEST1"
+
+#Pull List File from Github
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bghazel/bghazellearning.github.io/refs/heads/main/PowerShell/Toolbox/List.txt" -OutFile "C:\Toolbox\List.txt"
+
+Get-Content C:\Toolbox\List.txt | ForEach-Object {
+    Read-Host "processing Line $_"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bghazel/bghazellearning.github.io/refs/heads/main/PowerShell/Toolbox/$_.ps1" -OutFile "C:\Toolbox\$_.ps1"
+}
 
 
-$appname = "WingetInstall"
-
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bghazel/bghazellearning.github.io/refs/heads/main/PowerShell/Toolbox/$appname.ps1" -OutFile "C:\Toolbox\$appname.ps1"
