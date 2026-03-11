3b20e8321ade0f15606cb26f2f6f94d22c096c72
bghazel <ben.g.hazel7@gmail.com>
2026-03-11 10:06:36 -0400

    completed chapter 7 and created 8.

--- PowerShell/Powershell in a Month of Lunches/Chapter7.ps1
+++ PowerShell/Powershell in a Month of Lunches/Chapter7.ps1
@@ -17,4 +17,20 @@
         #there are quite a few including one for vscode.
         #it is a variable set by windows enviroment. Can be changed via control panel or gpo
         #put modules there and ps will auto discover htem and load them all the time. will also updated them when update-help is ran.
+        #find the path for them using properties in context menu.
+    #can load providers too.
+    #module commands tend to just add a prefix to their commands. like get-ADUser. Is best practice to avoid conflicts with other similar commands
+    # Console File 
+        #load in all the modules that you would like to have be default
+        #do export-console C:\path to wherever
+        # create powershell shortcut pointing to %windir%\system32\WindowsPowerShell\v1.0\powershell.exe -noexit -psconsolefile c:\myshell.psc
+        # This might be deprecated bc snapins are outdated and modules are auto loaded? ************** This is correct you can just use the profile variable/file. $Profile | Select-object * make a ps1 file with all the modules you want to load in
+#powershellget
+    #find-module to see list
+    #install-module to download one of them
+
+#Lab
+    #help *trouble* help *get-trouble* help *invoke-troub*.  shows the basic path for the trouble shootingpacks and how to use them. 
+    #Followed the path up until it selected audio. typed n and tabbed to networking. 
+    #then did Get-TroubleshootingPack 'C:\Windows\diagnostics\system\Networking' | Invoke-TroubleshootingPack and followed the instructions
 
